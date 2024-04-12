181d8d24126f324eca2240a7577ec8f91981279d
Akkel-i <118986817+Akkel-i@users.noreply.github.com>
2024-04-12 14:59:51 +0300

    added REST to 2 repo

---
+++ f1app/src/main/java/haagahelia/f1app/web/RestDriverController.java
@@ -0,0 +1,41 @@
+package haagahelia.f1app.web;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import haagahelia.f1app.domain.Driver;
+import haagahelia.f1app.domain.DriverRepository;
+
+@RestController
+public class RestDriverController {
+
+    @Autowired
+    private DriverRepository driverRepository;
+
+    @RequestMapping(value = "/drivers", method = RequestMethod.GET)
+    public @ResponseBody List<Driver> LapList() {
+
+        return (List<Driver>) driverRepository.findAll();
+    }
+
+        @RequestMapping(value="/driver/{id}", method = RequestMethod.GET)
+        public @ResponseBody Optional<Driver> finddDiverRest(@PathVariable("id") Integer driverId) {	
+            return driverRepository.findById(driverId);
+        }      
+        
+        // REST service to save new driver
+        @RequestMapping(value="/drivers", method = RequestMethod.POST)
+        public @ResponseBody Driver saveDriverRest(@RequestBody Driver newdriver) {	
+            return driverRepository.save(newdriver);
+        }
+
+
+}
---
+++ f1app/src/main/java/haagahelia/f1app/web/RestLapController.java
@@ -0,0 +1,42 @@
+package haagahelia.f1app.web;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import haagahelia.f1app.domain.Lap;
+import haagahelia.f1app.domain.LapRepository;
+
+@RestController
+public class RestLapController {
+
+    /* REST palvelut voisi listätä jokaiselle repositorylle erikseen, mutta nämä samat niihin tulisivat alkuksi, niin en niitä ole tehnyt */
+
+    @Autowired
+    private LapRepository lapRepository;
+
+    @RequestMapping(value = "/laps", method = RequestMethod.GET)
+    public @ResponseBody List<Lap> LapList() {
+
+        return (List<Lap>) lapRepository.findAll();
+    }
+
+    @RequestMapping(value="/lap/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Lap> finddDiverRest(@PathVariable("id") Integer lapId) {	
+        return lapRepository.findById(lapId);
+    }      
+    
+    // REST service to save new lap
+    @RequestMapping(value="/laps", method = RequestMethod.POST)
+    public @ResponseBody Lap savelapRest(@RequestBody Lap newlap) {	
+        return lapRepository.save(newlap);
+    }
+
+}
